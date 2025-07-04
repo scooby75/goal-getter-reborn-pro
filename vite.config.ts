3a4aa5306d98fb00b76b635e16acac0cc61c1305
scooby75 <45230721+scooby75@users.noreply.github.com>
2025-07-04 10:53:25 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -11,22 +10,21 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  publicDir: 'public',
+  publicDir: "public",
   build: {
     copyPublicDir: true,
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),
+        main: path.resolve(__dirname, "index.html"),
       },
     },
   },
-  assetsInclude: ['**/*.csv'],
+  assetsInclude: ["**/*.csv"],
 }));
